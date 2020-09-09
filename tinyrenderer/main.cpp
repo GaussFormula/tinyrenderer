2b3ce30379c7575329c56a0c4501bfe6b496a188
GaussFormula <c594794@hotmail.com>
2020-09-10 01:33:22 +0800

    Updated.

--- tinyrenderer/main.cpp
+++ tinyrenderer/main.cpp
@@ -8,40 +8,35 @@
 constexpr int width = 800;
 constexpr int height = 600;
 
-void line(int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color)
+void line(int x0, int y0, int x1, int y1, TGAImage& image,const TGAColor& color)
 {
     bool steep = false;
-    int dx = std::abs(x0 - x1);
-    int dy = std::abs(y0 - y1);
-    if (dx < dy)
-    {
+    if (std::abs(x0 - x1) < std::abs(y0 - y1)) {
         std::swap(x0, y0);
         std::swap(x1, y1);
         steep = true;
     }
-    if (x0 > x1)
-    {
+    if (x0 > x1) {
         std::swap(x0, x1);
         std::swap(y0, y1);
     }
-    float derror = std::abs(dy / (float)dx);
-    float error = 0;
+    int dx = x1 - x0;
+    int dy = y1 - y0;
+    int derror2 = std::abs(dy) * 2;
+    int error2 = 0;
     int y = y0;
-    for (int x = x0; x < x1; ++x)
-    {
-        if (steep)
-        {
+    for (int x = x0; x <= x1; x++) {
+        if (steep) {
             image.set(y, x, color);
         }
-        else
-        {
+        else {
             image.set(x, y, color);
         }
-        error += derror;
-        if (error > 0.5)
-        {
+        error2 += derror2;
+
+        if (error2 > dx) {
             y += (y1 > y0 ? 1 : -1);
-            error -= 1.0;
+            error2 -= dx * 2;
         }
     }
 }
@@ -49,10 +44,7 @@ void line(int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color)
 int main(int argc, char** argv)
 {
     TGAImage image(width, height, TGAImage::Format::RGB);
-    line(13, 20, 80, 40, image, TGAColor(255,0,0,255));
-    line(120, 113, 40, 80, image, TGAColor(255,255,255,255));
-    line(80, 40, 13, 20, image, TGAColor(255, 255, 255, 255));
-    image.flip_vertically();
-    image.write_tga_file("output.tga",false,false);
+    line(0, 0, 20,600, image,TGAColor(255, 255, 255, 255));
+    image.write_tga_file("output.tga");
     return 0;
 }
