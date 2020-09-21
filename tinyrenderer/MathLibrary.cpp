fbfa2e21fe2fe5bf5d71d96d729eda4c69ae86fd
GaussFormula <c594794@hotmail.com>
2020-09-21 15:27:25 +0800

    Updated.

--- tinyrenderer/MathLibrary.cpp
+++ tinyrenderer/MathLibrary.cpp
@@ -6,9 +6,5 @@
 
 namespace MathLibrary
 {
-    /*template<typename T>
-    vector3<T> cross(const vector3<T>& lhs, const vector3<T>& rhs)
-    {
-        return vector3<T>(lhs.y * rhs.z - lhs.z * rhs.y, lhs.z * rhs.x - lhs.x * rhs.z, lhs.x * rhs.y - lhs.y * rhs.x);
-    }*/
+
 }
--- tinyrenderer/MathLibrary.h
+++ tinyrenderer/MathLibrary.h
@@ -149,5 +149,9 @@ namespace MathLibrary
         return out;
     }
 
-    
+    template<class T>
+    MathLibrary::vector3<T> cross(const MathLibrary::vector3<T>& lhs, const MathLibrary::vector3<T>& rhs)
+    {
+        return MathLibrary::vector3<T>(lhs.y * rhs.z - lhs.z * rhs.y, lhs.z * rhs.x - lhs.x * rhs.z, lhs.x * rhs.y - lhs.y * rhs.x);
+    }
 }
