45682faa30d0917c7b8d62c377fe03b8c141564a
GaussFormula <c594794@hotmail.com>
2020-09-23 13:57:24 +0800

    Updated.

--- tinyrenderer/MathLibrary.cpp
+++ tinyrenderer/MathLibrary.cpp
@@ -23,5 +23,54 @@ namespace MathLibrary
         : x(v.x), y(v.y), z(v.z)
     {}
 
+    Matrix::Matrix(int r,int c):
+        m(std::vector<std::vector<float>>(r,std::vector<float>(c,0.0f))),
+        rows(r),cols(c)
+    {}
 
+    Matrix Matrix::identity(int dimensions)
+    {
+        Matrix E(dimensions, dimensions);
+        for (int i = 0; i < dimensions; ++i)
+        {
+            for (int j = 0; j < dimensions; j++)
+            {
+                E[i][j] = (i == j) ? 1.0f : 0.0f;
+            }
+        }
+        return E;
+    }
+
+    int Matrix::nrows()const
+    {
+        return rows;
+    }
+
+    int Matrix::ncols()const
+    {
+        return cols;
+    }
+
+    std::vector<float>& Matrix::operator[](const int i)
+    {
+        assert(i >= 0 && i < rows);
+        return m[i];
+    }
+
+    Matrix Matrix::operator*(const Matrix& rhs)
+    {
+        Matrix ret(rows, rhs.ncols());
+        for (int i = 0; i < rows; ++i)
+        {
+            for (int j = 0; j < rhs.ncols(); ++j)
+            {
+                ret[i][j] = 0.0f;
+                for (int k = 0; k < cols; ++k)
+                {
+                    ret[i][j] += m[i][k] * rhs.m[k][j];
+                }
+            }
+        }
+        return ret;
+    }
 }
